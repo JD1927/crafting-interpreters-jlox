ca13d9efe696c9d1d2bff43dad5405ec3476fb3f
Juan Aguirre <j1927aguirre@gmail.com>
2022-07-30 22:37:15 -0500

    Add number literals

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -68,9 +68,24 @@ public class Scanner {
         break;
       case '"': string(); break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+    // Look for fractional part
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+      while (isDigit(peek())) advance();
+    }
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -99,6 +114,15 @@ public class Scanner {
   private char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
